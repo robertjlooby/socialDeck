929432887a728702607a8ed4b36251f8b2574cd6
Robert Looby <robertl@cs.uchicago.edu>
2013-03-12 18:15:45 -0500

    updated mailer host

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,6 +14,7 @@ class PasswordResetsController < ApplicationController
 
   def update
   	@user = User.find_by_password_reset_token!(params[:id])
+    @user.account_confirmed = true
   	if @user.password_reset_sent_at < 1.hour.ago
   		redirect_to new_password_reset_path, :notice => "Password reset has expired."
   	elsif @user.update_attributes(params[:user])
