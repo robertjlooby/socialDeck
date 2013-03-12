eda5c8ccc6ad115553fa9cb6ed35fc930c9c6b58
Robert Looby <robertl@cs.uchicago.edu>
2013-03-12 18:03:10 -0500

    updated sessions to only allow confirmed users to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_username(params[:username])
-    if user.present?
+    if user.present? && user.confirmed_user
       if user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect_to user
